7da696599df14bc3cb5e61bd19b8f77f8397d80b
Pramod Thomas <pmod23@gmail.com>
2020-05-26 20:23:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  arr = [1,2]
+  arr = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
   arr
 end
 
